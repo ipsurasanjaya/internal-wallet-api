03666ba0a7d832beb896edbc648d1a4124797dd8
Sura Sanjaya <surasanjaya@gmail.com>
2024-10-19 22:15:14 +0700

    feat: add function to get wallet by id

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,4 +1,8 @@
 class Wallet < ApplicationRecord
     belongs_to :entity
     has_many :transactions
+
+    def self.find_wallet(id)
+        find(id)
+    end
 end
--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -3,7 +3,16 @@ require 'rails_helper'
 RSpec.describe Wallet, type: :model do
 
   context 'validations' do
-    it 'returns an error if wallet_id is nil' do
+    it 'returns error when wallet is not found' do
+      expect { Wallet.find_wallet(-1) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'returns wallet when the wallet exist' do
+      wallet = double('Wallet', id: 1, user_id: 1)
+      allow(Wallet).to receive(:find).with(1).and_return(wallet)
+
+      result = Wallet.find_wallet(1)
+      expect(result).to eq(wallet)
     end
   end
 end
